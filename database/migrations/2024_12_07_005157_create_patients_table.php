681f7168caba495e4cef24e0aa9ee4b7d0fdebb7
conferenceal <appconferenceal@gmail.com>
2024-12-07 08:19:13 +0700

    Clinic First

---
+++ database/migrations/2024_12_07_005157_create_patients_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string('national_id',16)->unique();
+            $table->string('name',50);
+            $table->enum('gender',['Male','Female']);
+            $table->date('date_of_birth');
+            $table->string('phone_number',15);
+            $table->text('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('patients');
+    }
+};
---
+++ database/migrations/2024_12_07_010630_create_doctors_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->string('practice_license',20)->unique();
+            $table->string('name',50);
+            $table->string('phone_number',15);
+            $table->text('adrress');
+            $table->foreignId('specialty_id')->constrained('specialties')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doctors');
+    }
+};
